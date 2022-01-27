7f7a9860f268888e869f9e4101c2a496811efa3c
Junichi Kato <j5ik2o@gmail.com>
2022-01-27 09:18:45 +0900

    Update ClosedRangeSpec.scala

--- src/test/scala/tdd/ClosedRangeSpec.scala
+++ src/test/scala/tdd/ClosedRangeSpec.scala
@@ -3,7 +3,8 @@ package tdd
 import org.scalatest.freespec.AnyFreeSpec
 
 /**
- * https://gist.github.com/twada/75fb219c8cc180e9de166d8a58e877b0
+ * お題: https://gist.github.com/twada/75fb219c8cc180e9de166d8a58e877b0
+ * 動画： https://dxd2021.cto-a.org/program/time-table/a-1
  */
 class ClosedRangeSpec extends AnyFreeSpec {
 
